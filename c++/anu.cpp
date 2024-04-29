c09c0797f0955b4ac7778c42327950dda12489e2
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-04-29 11:41:08 +0530

    opps

--- c++/anu.cpp
+++ c++/anu.cpp
@@ -1,27 +1,62 @@
-#include<iostream>
-using namespace std;
-class Emp{
-   public:
-   char location;
-   string name;
-   int id;
-   void mdetails(string name){
-      cout<<name<<endl;}
-   void mdetails(int id){
-      cout<<id<<endl;}
-   void mdetails(char location){
-      cout<<location<<endl;}
-   void mdetails(string name,int id,char loctaion){
-       cout<<name<<endl;
-       cout<<id<<endl;
-       cout<<location<<endl;
-       cout<<"Regular checking ";
-   } 
+#include <iostream>
+
+class Employee {
+public:
+  int id;
+  std::string name;
+  Employee(int id, std::string name) {
+    this->id = id;
+    this->name = name;
+  }
+  virtual float sal_process() = 0;
 };
-int main(){
-   Emp a;
-   a.mdetails("Anurag");
-   a.mdetails(174);
-   a.mdetails('d');
-   a.mdetails("Anurag",174,'d');
+
+class EmpSalary : public Employee {
+public:
+  float salary;
+  EmpSalary(int id, std::string name, float salary) : Employee(id, name) {
+    this->salary = salary;
+  }
+  float sal_process() override { return salary; }
+};
+
+class Freelancer : public Employee {
+public:
+  float rate;
+  float hours;
+  Freelancer(int id, std::string name, float rate, float hours)
+      : Employee(id, name) {
+    this->rate = rate;
+    this->hours = hours;
+  }
+  float sal_process() override { return hours * rate; }
+};
+
+class Commission : public EmpSalary {
+  float commission;
+
+public:
+  Commission(int id, std::string name, float salary, float commission)
+      : EmpSalary(id, name, salary) {
+    this->commission = commission;
+  }
+  float sal_process() override { return salary + commission; }
+};
+
+class PayRoll_Process {
+public:
+  float sal_process(Employee &emp, Freelancer &f, Commission &c) {
+    return emp.sal_process() + f.sal_process() + c.sal_process();
+  }
+};
+
+int main() {
+  EmpSalary emp1(1, "John", 1000);
+  Freelancer emp2(2, "Jane", 10, 100);
+  Commission emp3(3, "Jim", 1000, 100);
+  std::cout << emp1.sal_process() << std::endl;
+  std::cout << emp2.sal_process() << std::endl;
+  std::cout << emp3.sal_process() << std::endl;
+  PayRoll_Process p;
+  std::cout << p.sal_process(emp1, emp2, emp3);
 }
