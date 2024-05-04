9489116a8941b61f555275cbe31dd642bb4536cb
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-05-04 10:51:41 +0530

    Prime number

--- JAVASCRIPT/anu.js
+++ JAVASCRIPT/anu.js
@@ -5,3 +5,14 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 console.log(fib(10))
+// orime number
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+  return true;
+}
+console.log(isPrime(11))
