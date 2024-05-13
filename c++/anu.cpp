37e8b71e400baf8a6fbaa904932a44ee98f0a61d
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-05-13 14:56:03 +0530

    regular checking

--- c++/anu.cpp
+++ c++/anu.cpp
@@ -1,62 +1,24 @@
 #include <iostream>
-
-class Employee {
-public:
-  int id;
-  std::string name;
-  Employee(int id, std::string name) {
-    this->id = id;
-    this->name = name;
-  }
-  virtual float sal_process() = 0;
+using  namespace std;
+class Test {
+	int num1, num2, num3;
+	public:
+		Test(int value1 = 10, int value2 = 20, int value3 = 30) { // Parameterized constructor with default arguments
+			num1 = value1;
+			num2 = value2;
+			num3 = value3;
+		}
+		void display() {
+			cout << num1 << " " << num2 << " " << num3 << endl;
+		}
 };
-
-class EmpSalary : public Employee {
-public:
-  float salary;
-  EmpSalary(int id, std::string name, float salary) : Employee(id, name) {
-    this->salary = salary;
-  }
-  float sal_process() override { return salary; }
-};
-
-class Freelancer : public Employee {
-public:
-  float rate;
-  float hours;
-  Freelancer(int id, std::string name, float rate, float hours)
-      : Employee(id, name) {
-    this->rate = rate;
-    this->hours = hours;
-  }
-  float sal_process() override { return hours * rate; }
-};
-
-class Commission : public EmpSalary {
-  float commission;
-
-public:
-  Commission(int id, std::string name, float salary, float commission)
-      : EmpSalary(id, name, salary) {
-    this->commission = commission;
-  }
-  float sal_process() override { return salary + commission; }
-};
-
-class PayRoll_Process {
-public:
-  float sal_process(Employee &emp, Freelancer &f, Commission &c) {
-    return emp.sal_process() + f.sal_process() + c.sal_process();
-  }
-};
-
 int main() {
-  EmpSalary emp1(1, "John", 1000);
-  Freelancer emp2(2, "Jane", 10, 100);
-  Commission emp3(3, "Jim", 1000, 100);
-  std::cout << emp1.sal_process() << std::endl;
-  std::cout << emp2.sal_process() << std::endl;
-  std::cout << emp3.sal_process() << std::endl;
-  PayRoll_Process p;
-  std::cout << p.sal_process(emp1, emp2, emp3);
+	Test t1(25, 55, 75);
+	t1.display(); // Statement-1
+	Test t2(24, 25);
+	t2.display();// Statement-2
+	Test t3(44);
+	t3.display();// Statement-3
+	Test t4();
+	t4.display();// Statement-4
 }
