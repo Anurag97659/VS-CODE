15b0f3c409d484cfb610c1aaaf617bcb37a57105
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-06-07 09:11:51 +0530

    pulling

--- c++/anu.cpp
+++ c++/anu.cpp
@@ -1,43 +1,11 @@
 #include <iostream>
 using namespace std;
-
-class School{
-	public:
-	string schoolname;
-	string location;
-	School(string name, string loc){
-		this-> schoolname = name;
-		this-> location=loc;}
-};
-class Class{
-		public:
-		int classnumber;
-		string section;
-		Class(int num, string sec){
-			this-> classnumber = num;
-			this-> section=sec;
-		}
-};
-class Student{
-	public:
-	string name;
-	int rollno;
-	Student(string name, int rollno){
-		this-> name = name;
-		this-> rollno=rollno;
-	}
-	void display(School &s, Class &c){
-		cout<<"Name: "<<name<<endl;
-		cout<<"Roll No: "<<rollno<<endl;
-		cout<<"Class: "<<c.classnumber<<endl;
-		cout<<"Section: "<<c.section<<endl;					
-		cout<<"School Name: "<<s.schoolname<<"  ";
-		cout<<"Location: "<<s.location<<endl;
-	}
-};
 int main(){
-	School s("APS","Dk");
-	Class c(12,"C");
-	Student st("Anurag",10);
-	st.display(s,c);
+   int a[5]={5,6,2,3,14};
+   int b=0;
+   for(int i=0;i<5;i++){
+	if(a[i]>b){
+	   b=a[i];}
+   }
+   cout<<b;
 }
