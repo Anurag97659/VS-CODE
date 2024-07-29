3570b427dd930f79e4b226989f300c77e401784d
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-07-29 20:19:46 +0530

    java file

--- JAVA/anu.java
+++ JAVA/anu.java
@@ -1,8 +1,32 @@
-
-public class anu  {
+import java.util.Scanner;
+public class anu {
+    static int logic(int x, int y){
+        int z;
+        if(x>y){
+            z = x+y;
+        }
+        else {
+            z = (x +y) * 5;
+        }
+        x = 566;
+        return z;
+    }
     public static void main(String[] args) {
-       
-        System.out.printf("Hello , i am Anurag nidhi");
-       
+        int a = 5;
+        int b = 7;
+        int c;
+        c = logic(a, b);
+        System.out.println(a + " "+ b);
+        int a1 = 2;
+        int b1 = 1;
+        int c1;
+        c1 = logic(a1, b1);
+        System.out.println(c);
+        System.out.println(c1);
+        System.out.println( logic(a1, b1));
+        // Scanner d=new Scanner(System.in);
+        // System.out.println("enter number = ");
+        // int f=d.nextInt();
+        // System.out.println(f);
     }
 }
