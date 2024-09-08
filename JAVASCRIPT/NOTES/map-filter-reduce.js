b8b2152c047d1134e92c1d8229638083341a0a9c
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-09-08 17:11:55 +0530

    js notes DOM

--- JAVASCRIPT/NOTES/map-filter-reduce.js
+++ JAVASCRIPT/NOTES/map-filter-reduce.js
@@ -1,28 +1,28 @@
-// // ----------------------------------------------MAP FILTER REDUCE -----------------------------------------//
-
-// let arr = [45, 23, 21]
+// // =========================================== MAP =======================================
 
 // // Array map method ---- > creates a new array based on some condition
-
+// let arr = [45, 23, 21]
 // let a = arr.map((value, index, array) => {
-//   console.log(value, index, array)
-//   return value + index
-// })
-
-// console.log(arr)
-
+//       console.log(value, index, array)
+//       return value + index
+//     })
+//     console.log(a)
+    
+    
+// // =========================================== FILTER =======================================
+    
 // // Array filter method ---> checks condition for each element and returns elements which pass the condition
-
 // let arr2 = [45, 23, 21, 0, 3, 5]
 // let a2 = arr2.filter((a) => {
-//   return a < 10
-// })
-
+//           return a < 10
+//     })
 // console.log(a2, arr2)
+   
+        
 
-// // // Array reduce method    --------> basically it takes a callback function which reduces to one value
-
-// let arr3 = [1, 2, 3, 5, 2, 1]
+// // =========================================== REDUCE =======================================
+// // Array reduce method    --------> basically it takes a callback function which reduces to one value
+// let arr3 = [1, 2, 3, 4, 5, 6,7,8,9,10]
 // const reduce_func = (h1, h2) => {
 //   return h1 + h2
 // }
