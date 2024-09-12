2fd1131c0b067338a49e25cea6cfac521b52ae52
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-09-12 21:42:02 +0530

    dsa removed

--- c++/anu.cpp
+++ c++/anu.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
-    int arr[]={1,2,3,4,5};
-   
-    for(int i=0;i<5;i++){
-        cout<<arr[i]<<endl;
+    int arr[10]={1,1,1,2,2,3,3,3,4,4};
+    int hash[4]={0};
+    for(int i=0;i<10;i++){
+        hash[arr[i]]++;
     }
+    // for(int i=0;i<7;i++){
+    //     cout<<i<<" "<<hash[i]<<endl;
+    // }
+    for(int i=0;i<4;i++){
+        if((hash[i]%2) != 0){
+            cout<<i<<" ";
+        }
+    }    
 }
