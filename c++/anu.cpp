8c785a4429a76e60716b6571b16a56b597ae36b9
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-09-16 19:00:42 +0530

    linklist

--- c++/anu.cpp
+++ c++/anu.cpp
@@ -1,17 +1,55 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct node {
+ int data;
+ struct node *next;
+};
+struct node *head = NULL;
+struct node *current = NULL;
+// display the list
+void printList(){
+ struct node *p = head;
+ printf("\n[");
+ 
+ //start from the beginning
+ while(p != NULL) {
+ printf(" %d ",p->data);
+ p = p->next;
+ }
+ printf("]");
+}
+//insertion at the beginning
+void insertatbegin(int data){
+ //create a link
+ struct node *lk = (struct node*) malloc(sizeof(struct node));
+ lk->data = data;
+ // point it to old first node
+ lk->next = head;
+ 
+ //point first to new first node
+ head = lk;
+}
+void insertatend(int data){
+ //create a link
+ struct node *lk = (struct node*) malloc(sizeof(struct node));
+ lk->data = data;
+ struct node *linkedlist = head;
+ // point it to old first node
+ while(linkedlist->next != NULL)
+ linkedlist = linkedlist->next;
+ //point first to new first node
+ linkedlist->next = lk;
+}
 int main(){
-    int arr[10]={1,1,1,2,2,3,3,3,4,4};
-    int hash[4]={0};
-    for(int i=0;i<10;i++){
-        hash[arr[i]]++;
-    }
-    // for(int i=0;i<7;i++){
-    //     cout<<i<<" "<<hash[i]<<endl;
-    // }
-    for(int i=0;i<4;i++){
-        if((hash[i]%2) != 0){
-            cout<<i<<" ";
-        }
-    }    
+ int k=0;
+ insertatbegin(12);
+ insertatend(22);
+ insertatend(30);
+ insertatend(44);
+ insertatend(50);
+ printf("Linked List: ");
+ 
+ // print list
+ printList();
 }
