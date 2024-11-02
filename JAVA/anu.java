74c9a943529b2e8ec9d81d97108a4a56f9b60b75
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-11-02 21:26:12 +0530

    nothing

--- JAVA/anu.java
+++ JAVA/anu.java
@@ -1,39 +1,75 @@
+class TimeManagement {
+    int hours;
+    int minutes;
+    int seconds;
 
-class BankAccount{
-    public int Account_number;
-    public String Account_holder_name;
-    public String Account_type ;
-    public int Account_balance;
-    public void setAccountDetails( int Account_number, String Account_holder_name, String Account_type, int Account_balance){
-        this.Account_number=Account_number;
-        this.Account_holder_name=Account_holder_name;
-        this.Account_type=Account_type;
-        this.Account_balance=Account_balance;
+    TimeManagement(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
     }
-    public void deposit(int deposit){
-        this.Account_balance+=deposit;
+
+    TimeManagement(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
     }
-    public void withdraw(int withdraw){
-        this.Account_balance-=withdraw;
+
+    TimeManagement(int hours) {
+        this.hours = hours;
     }
-    public void checkblance(){
-        System.out.println("Account holder name = "+Account_holder_name);
-        System.out.println("Account number = "+Account_number);
-        System.out.println("Account type = "+Account_type);
-        System.out.println("Account balance = "+Account_balance);
-        System.out.println("");
+
+    void add(int hours, int minutes, int seconds) {
+        this.hours += hours;
+        this.minutes += minutes;
+        this.seconds += seconds;
     }
+
+    void add(int hours, int minutes) {
+        this.hours += hours;
+        this.minutes += minutes;
+    }
+
+    void add(int hours) {
+        this.hours += hours;
+    }
+
+    void subtract(int hours, int minutes, int seconds) {
+        this.hours -= hours;
+        this.minutes -= minutes;
+        this.seconds -= seconds;
+    }
+
+    void subtract(int hours, int minutes) {
+        this.hours -= hours;
+        this.minutes -= minutes;
+    }
+
+    void subtract(int hours) {
+        this.hours -= hours;
+    }
+
+    
 }
+public class anu{
+    public static void main(String[] args) {
+        
+        TimeManagement t = new TimeManagement(10, 30, 45);
+        t.add(2, 15, 30);
+        System.out.println("Time after adding 2 hours, 15 minutes, and 30 seconds: " + t.hours + " hours " + t.minutes + " minutes " + t.seconds + " seconds");
+        t.subtract(1, 10, 15);
+        System.out.println("Time after subtracting 1 hour, 10 minutes, and 15 seconds: " + t.hours + " hours " + t.minutes + " minutes " + t.seconds + " seconds");
 
-public class anu {
-    public static void main(String [] args){
-       BankAccount a = new BankAccount();
-       a.setAccountDetails(1234567890,"Anurag_Nidhi", "savings", 10);
-       a.checkblance();
-       a.deposit(90);
-       a.checkblance();
-       a.withdraw(10);
-       a.checkblance();
-      
+        TimeManagement t2 = new TimeManagement(10, 30);
+        t2.add(2, 15);
+        System.out.println("Time after adding 2 hours and 15 minutes: " + t2.hours + " hours " + t2.minutes + " minutes");
+        t2.subtract(1, 10);
+        System.out.println("Time after subtracting 1 hour and 10 minutes: " + t2.hours + " hours " + t2.minutes + " minutes");
+
+        TimeManagement t3 = new TimeManagement(10);
+        t3.add(2);
+        System.out.println("Time after adding 2 hours: " + t3.hours + " hours");
+        t3.subtract(1);
+        System.out.println("Time after subtracting 1 hour: " + t3.hours + " hours");
+
     }
 }
--- c++/anu.cpp
+++ c++/anu.cpp
@@ -1,96 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-struct node{
-    int data;
-    struct node *next;
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int a =-1;
+         int size = sizeof(arr) / sizeof(arr[0]);
+        for(int i=0;i<size;i++){
+            cout <<i<<endl;
+            if(arr[i]>=a){
+                a=arr[i];
+            }
+            cout<<a<<endl;
+        }
+        return a;
+    }
 };
-struct node *head= NULL;
-
-void display(){
-    struct node *display = head;
-    while(display != NULL){
-        cout<<display->data<<" ";
-        display=display->next;
-    }cout<<endl;
-}
-
-void addatStart(int data){
-    struct node *temp =(struct node*) malloc(sizeof(struct node));
-    temp->data=data;
-    temp->next=head;
-    head=temp;
-}
-
-void deleteatStart(){
-    struct node *del = head;
-    head = head->next;
-    delete del;
-}
-
-void addatEnd(int data){
-    struct node *temp =(struct node*)malloc(sizeof(struct node));
-    temp->data=data;
-    struct node *tempHead = head;
-    while(tempHead->next!=NULL){
-        tempHead=tempHead->next;
-    }
-    tempHead->next=temp;
-    temp->next=NULL;
-}
-void deleteatEnd(){
-    struct node *tempHead =head;
-    int i=0;
-    while(tempHead->next!=NULL){
-        tempHead=tempHead->next;
-        i++;
-    }
-    struct node *del =tempHead;
-    tempHead = head;
-    i=i-1;
-    int a=0;
-    while( tempHead->next!=NULL && a!=i){
-            tempHead=tempHead->next;
-            a++;
-    }
-    tempHead->next=NULL;
-    delete del;
-}
-void addatINbetween(int data,int a){
-    struct node *temp = (struct node*)malloc(sizeof(struct node));
-    temp->data=data;
-    struct node *tempHead =head;
-    int i=0;
-    while(tempHead->next!=NULL && i!=a){
-        tempHead=tempHead->next;
-        i++;
-    }
-    struct node *insert = (struct node*)malloc(sizeof(struct node));
-    insert->next=tempHead->next;
-    tempHead->next=temp;
-    temp->next=insert->next;
-}
-void deleteatInbetween(int a){
-    struct node *tempHead = head;
-    int i=0;
-    while(tempHead->next!=NULL && i!=a-1){
-        tempHead=tempHead->next;
-        i++;
-    }
-    struct node *del = tempHead->next;
-    tempHead->next=tempHead->next->next;
-    delete del;
-}
-
 int main(){
-    addatStart(5);
-    addatStart(15);
-    addatStart(50);
-    addatStart(150);
-    addatEnd(100);
-    addatINbetween(55,2);
-    display();
-    deleteatInbetween(3);
-    display();
-
+   vector<int> arr({1,2,3,4,56,22,222});
+   Solution a;
+   a.largest(arr);
 }
+
+    
