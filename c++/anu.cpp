2bd4bc3c07e5d10b281a36d79a9cc737d96dd199
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-11-18 22:49:33 +0530

    linked list

--- c++/anu.cpp
+++ c++/anu.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
-#include <vector>
 using namespace std;
- class Solution{
+class Node{
     public:
-    int getFloorAndCeil(vector<int> &a, int n, int x) {
-	 int size = a.size();
-        int low=0; int high =size-1;
-        int ceil = -1; int floor =-1;
-        while(low<=high){
-            int mid =(low+high)/2;
-			if(a[mid]==x){
-				floor = a[mid];
-				ceil = a[mid];
-                break;
-			}
-            else if(a[mid]>x){
-                ceil = a[mid];
-                high = mid-1;
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class Solution {
+  public:
+  Node *head;
+    Node *insertAtEnd(Node *head, int x) {
+         this->head = head;
+         Node* temp = head;
+         Node* newnode = new Node(x);
+         while(temp->next!=NULL){
+             temp=temp->next;
+         }
+         temp->next= newnode;
+    }
+
+    Node *display(){
+            Node* temp1 = head;
+            while(temp1!=NULL){
+                cout<<temp1->data<<" ";
+                temp1=temp1->next;
             }
-            else{
-				floor = a[mid];
-                low =mid+1;
-            }
-        
-        }
-		cout<<floor<<" "<<ceil<<endl;
-        return  ceil, floor;
+    }
+};
+int main(){
+     Solution a;
+        Node* head = new Node(10);
+        a.insertAtEnd(head,1550);
+        a.insertAtEnd(head,20);
+        a.insertAtEnd(head,30);
+        a.display();
 }
- } ;
- int main(){
-        Solution obj;
-        vector<int> a = {1, 2, 8, 10, 10, 12, 19};
-        int n = 7;
-        int x = 10;
-        obj.getFloorAndCeil(a, n, x);
-        return 0;
- }
--- c++/linkedlist.cpp
+++ c++/linkedlist.cpp
@@ -8,48 +8,50 @@ struct node {
 };
 
 struct node *head = NULL;
-struct node *current = NULL;
 
 void printList(){
- struct node *p = head;
- cout << "\n[";
- while(p != NULL) {
- cout << " " << p->data << " ";
- p = p->next;
- }
- cout << "]";
+    struct node *p = head;
+    cout << "\n[";
+    while(p != NULL) {
+        cout << " " << p->data << " ";
+        p = p->next;
+    }
+    cout << "]";
 }
+
 void reverse(){
     struct node *prev = NULL;
     struct node *temp = head;
     struct node *k;
     while(temp != NULL){
-    k = temp->next;
-    temp->next = prev;
-    prev = temp;
-    temp = k;
+        k = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = k;
     }
     head = prev;
 }
+
 void insertatbegin(int data){
- struct node *lk = (struct node*) malloc(sizeof(struct node));
- lk->data = data;
- lk->next = head;
- head = lk;
+    struct node *lk = (struct node*) malloc(sizeof(struct node));
+    lk->data = data;
+    lk->next = head;
+    head = lk;
 }
 
 void insertbed(int data,int x){
- struct node *lk = (struct node*) malloc(sizeof(struct node));
- lk->data = data;
- struct node *linkedlist = head;
- int i=0;
- while(linkedlist->next != NULL && i!=x){
- linkedlist = linkedlist->next;
- i++;}
- struct node *temp=(struct node*) malloc(sizeof(struct node));
- temp->next = linkedlist->next;
- linkedlist->next = lk;
- lk->next =temp->next;
+    struct node *lk = (struct node*) malloc(sizeof(struct node));
+    lk->data = data;
+    struct node *linkedlist = head;
+    int i=0;
+    while(linkedlist->next != NULL && i!=x){
+        linkedlist = linkedlist->next;
+         i++;
+    }
+    struct node *temp=(struct node*) malloc(sizeof(struct node));
+    temp->next = linkedlist->next;
+    linkedlist->next = lk;
+    lk->next =temp->next;
 }
 
 void insertatend(int data){
