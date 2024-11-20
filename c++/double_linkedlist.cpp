b803a3da578b39df1c328409abfe1eacf20897c0
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2024-11-20 19:44:02 +0530

    addatstart and addatany added in double-linked-list

--- c++/double_linkedlist.cpp
+++ c++/double_linkedlist.cpp
@@ -38,12 +38,49 @@ void addatend(int data){
      }
 }
 
+void addatstart(int data){
+    struct node* temp = new node();
+    temp->data=data;
+    temp->next=head;
+    if(head!=NULL){
+    head->prev=temp;
+    }
+    head=temp;
+    head->prev=NULL;
+    struct node* settail= head;
+    while(settail->next!=NULL){
+        settail=settail->next;
+    }
+    tail=settail;
+}
+
+void addatany(int data,int where){
+    struct node* temp =new node();
+    struct node* before=head;
+    temp->data=data;
+    int i=0;
+    while(i!=where-1){
+        before=before->next;i++;
+    }
+    struct node* after=before->next;
+    after->prev=temp;
+    temp->next=before->next;
+    before->next=temp;
+    temp->prev=before;
+}
+
 int main(){
-    addatend(101);
-    addatend(202);
-    addatend(303);
-    addatend(404);
-    addatend(505);
+    // addatend(101);
+    // addatend(202);
+    // addatend(303);
+    // addatend(404);
+    // addatend(505);
+    addatstart(10);
+    addatstart(20);
+    addatstart(30);
+    addatstart(40);
+    addatstart(50);
+    addatany(35,2);
     printList();
     cout<<endl;
    cout<<endl<<"head = "<<head->data<<"              tail = "<<tail->data;
@@ -51,5 +88,6 @@ int main(){
    cout<<endl<<"2 = "<<head->next->next->data<<"              2 = "<<tail->prev->prev->data;
    cout<<endl<<"3 = "<<head->next->next->next->data<<"              1 = "<<tail->prev->prev->prev->data;
    cout<<endl<<"4 = "<<head->next->next->next->next->data<<"              0 = "<<tail->prev->prev->prev->prev->data;
+   
    return 0;
 }
