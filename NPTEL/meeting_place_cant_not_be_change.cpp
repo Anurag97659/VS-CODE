9d23b214c838974dd05900820110e8fb7d1f5580
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2026-07-31 19:55:16 +0530

    the meeting place cannot be changed 💀😭💀

---
+++ NPTEL/meeting_place_cant_not_be_change.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <cstdlib>
+
+int main(){
+    
+    // system("start .\\Notes\\meeting_place_explained.html");
+    system("start .\\Notes\\meeting_place_explained.html");
+}
